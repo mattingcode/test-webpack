beea841c572c15cd74ebee84e92325543e5e808d
mattingcode <ma_83814393@163.com>
2020-08-04 22:34:25 +0800

    tr

--- matt-builder-webpack/test/smoke/index.js
+++ matt-builder-webpack/test/smoke/index.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
 const rimraf = require('rimraf');
+const Mocha = require('mocha');
+
+const mocha = new Mocha({
+  timeout: '10000ms'
+});
 
 process.chdir(path.join(__dirname, 'template'));
 
 rimraf('./dist', () => {
   const prodConfig = require('../../lib/webpack.prod.js');
   webpack(prodConfig, (err, stats) => {
+    console.log('stats');
     if (err) {
-      console.error(err);
+      console.error('err', err);
       process.exit(2);
     }
     console.log(stats.toString({
@@ -16,5 +22,8 @@ rimraf('./dist', () => {
       modules: false,
       children: false
     }))
+    mocha.addFile(path.join(__dirname, 'html-test.js'));
+    mocha.addFile(path.join(__dirname, 'css-js-test.js'));
+    mocha.run();
   })
 });
